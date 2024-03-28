0029847121f4ccf837574b848e9c0bcd5d1ce5ff
F286 <F286@users.noreply.github.com>
2024-03-28 09:34:22 +0900

    update

--- sdf/sdf_model_conv_next.py
+++ sdf/sdf_model_conv_next.py
@@ -7,85 +7,106 @@ class SDFNet(nn.Module):
         super(SDFNet, self).__init__()
 
         # Constants for sizes
-        INITIAL_FEATURES = 8
-        MIDDLE_SIZE = 16
+        LAYER_FEATURES = [16, 32, 64]  # This array directly specifies the number of features per layer
+        # LAYER_FEATURES = [8, 16, 32, 64, 128, 256]  # This array directly specifies the number of features per layer
         FEED_FORWARD_EXPAND = 4
-        KERNEL_SIZE = 7
-        INITIAL_EXTENTS = 128
-        DOWNSCALE_LAYERS = 5
+        KERNEL_SIZE = 3 # 7
+        INPUT_EXTENTS = 128
+        INITIAL_EXTENTS = 32
+        DOWNSCALE_LAYERS = len(LAYER_FEATURES) - 1  # The size of LAYER_FEATURES array sets DOWNSCALE_LAYERS
         MIDDLE_EXTENTS = INITIAL_EXTENTS // 2**DOWNSCALE_LAYERS
-
-        # TODO: The convnext actually does at 2x2 with a stride of 2 to downsample at the very start. can do that
-        # TODO: Before any downsample, : one before each downsampling layer, one after the stem.
-        # This very first downsample seems to be the 'stem'
 
         # Initial Convolution
         self.initial_conv = nn.Sequential(
-            nn.Conv2d(1, INITIAL_FEATURES, kernel_size=1, padding="same", padding_mode="replicate", bias=False),
-            nn.LayerNorm([INITIAL_FEATURES, INITIAL_EXTENTS, INITIAL_EXTENTS])
+            nn.Conv2d(1, LAYER_FEATURES[0], kernel_size=1, padding="same", padding_mode="replicate", bias=False),
+            nn.LayerNorm([LAYER_FEATURES[0], INPUT_EXTENTS, INPUT_EXTENTS])
         )
 
         # Contracting Path (Downscaling)
         self.down_convs = nn.ModuleList()
+        self.downscale_convs = nn.ModuleList()
+
+        # Stem (initial downscale) 
+        # TODO Add stem for convnext, initial downscale. append it manually, and use a 4x4 downscale.
+
+        # Stem (initial downscale) 
+        # Implementing the stem for an initial downscale using a 4x4 stride to effectively reduce the spatial dimensions
+        self.down_convs.append(nn.Sequential())
+        self.downscale_convs.append(
+            nn.Sequential(
+                nn.Conv2d(LAYER_FEATURES[0], LAYER_FEATURES[0], kernel_size=4, stride=4, padding=0, bias=False)
+            ))
+
         for i in range(DOWNSCALE_LAYERS):
             current_extents = INITIAL_EXTENTS // 2**i
-            print(current_extents)
+            features_in = LAYER_FEATURES[i]
+            features_out = LAYER_FEATURES[i + 1]
+            print(f"current_extents {features_in}  features_out {features_out}")
             self.down_convs.append(
                 nn.Sequential(
                     # Depthwise convolution
-                    nn.Conv2d(INITIAL_FEATURES, INITIAL_FEATURES, kernel_size=KERNEL_SIZE, padding="same", groups=INITIAL_FEATURES, bias=False),
-                    nn.LayerNorm([INITIAL_FEATURES, current_extents, current_extents]),
+                    nn.Conv2d(features_in, features_in, kernel_size=KERNEL_SIZE, padding="same", groups=features_in, bias=False),
+                    nn.LayerNorm([features_in, current_extents, current_extents]),
                     # Pointwise convolution to expand the channel size
-                    nn.Conv2d(INITIAL_FEATURES, INITIAL_FEATURES * FEED_FORWARD_EXPAND, kernel_size=1, bias=False),
+                    nn.Conv2d(features_in, features_in * FEED_FORWARD_EXPAND, kernel_size=1, bias=False),
                     nn.GELU(),
                     # Feed-forward layer to contract back to the initial number of features
-                    nn.Conv2d(INITIAL_FEATURES * FEED_FORWARD_EXPAND, INITIAL_FEATURES, kernel_size=1, bias=False)
+                    nn.Conv2d(features_in * FEED_FORWARD_EXPAND, features_out, kernel_size=1, bias=False)
                 )
             )
-        self.downscale_convs = nn.ModuleList([
-            nn.Sequential(
-                nn.Conv2d(INITIAL_FEATURES, INITIAL_FEATURES, kernel_size=2, stride=2, padding=0, bias=False)
-            ) for _ in range(DOWNSCALE_LAYERS)
-        ])
-            
+            self.downscale_convs.append(
+                nn.Sequential(
+                    nn.Conv2d(features_out, features_out, kernel_size=2, stride=2, padding=0, bias=False)
+                ))
+
         # Middle Part
         self.middle_convs = nn.Sequential(
-                nn.Sequential(
-                    # Depthwise convolution
-                    nn.Conv2d(INITIAL_FEATURES, MIDDLE_SIZE, kernel_size=KERNEL_SIZE, padding="same", groups=INITIAL_FEATURES, bias=False),
-                    nn.LayerNorm([MIDDLE_SIZE, MIDDLE_EXTENTS, MIDDLE_EXTENTS]),
-                    # Pointwise convolution to expand the channel size
-                    nn.Conv2d(MIDDLE_SIZE, MIDDLE_SIZE * FEED_FORWARD_EXPAND, kernel_size=1, bias=False),
-                    nn.GELU(),
-                    # Feed-forward layer to contract back to the initial number of features
-                    nn.Conv2d(MIDDLE_SIZE * FEED_FORWARD_EXPAND, INITIAL_FEATURES, kernel_size=1, bias=False)
-                )
+            nn.Sequential(
+                # Depthwise convolution
+                nn.Conv2d(LAYER_FEATURES[-1], LAYER_FEATURES[-1], kernel_size=KERNEL_SIZE, padding="same", groups=LAYER_FEATURES[-1], bias=False),
+                nn.LayerNorm([LAYER_FEATURES[-1], MIDDLE_EXTENTS, MIDDLE_EXTENTS]),
+                # Pointwise convolution to expand the channel size
+                nn.Conv2d(LAYER_FEATURES[-1], LAYER_FEATURES[-1] * FEED_FORWARD_EXPAND, kernel_size=1, bias=False),
+                nn.GELU(),
+                # Feed-forward layer to contract back to the initial number of features
+                nn.Conv2d(LAYER_FEATURES[-1] * FEED_FORWARD_EXPAND, LAYER_FEATURES[-1], kernel_size=1, bias=False)
+            )
         )
 
         # Expanding Path (Upscaling)
         self.up_transposes = nn.ModuleList()
         self.up_convs = nn.ModuleList()
+
         for i in range(DOWNSCALE_LAYERS):
+            index = DOWNSCALE_LAYERS - 1 - i
+            features_in = LAYER_FEATURES[index + 1]
+            features_out = LAYER_FEATURES[index]
+            print(f"current_extents {features_in}  features_out {features_out}")
+            
             self.up_transposes.append(
-                nn.ConvTranspose2d(INITIAL_FEATURES, INITIAL_FEATURES, kernel_size=2, stride=2, bias=False)
+                nn.ConvTranspose2d(features_in, features_in, kernel_size=2, stride=2, bias=False)
             )
-            current_extents = INITIAL_EXTENTS // 2**(DOWNSCALE_LAYERS - 1 - i)
+            current_extents = INITIAL_EXTENTS // 2**index
             print(current_extents)
             self.up_convs.append(
                 nn.Sequential(
                     # Depthwise convolution
-                    nn.Conv2d(INITIAL_FEATURES * 2, INITIAL_FEATURES, kernel_size=KERNEL_SIZE, padding="same", groups=INITIAL_FEATURES, bias=False),
-                    nn.LayerNorm([INITIAL_FEATURES, current_extents, current_extents]),
+                    nn.Conv2d(features_in * 2, features_out, kernel_size=KERNEL_SIZE, padding="same", groups=features_out, bias=False),
+                    nn.LayerNorm([features_out, current_extents, current_extents]),
                     # Pointwise convolution to expand the channel size
-                    nn.Conv2d(INITIAL_FEATURES, INITIAL_FEATURES * FEED_FORWARD_EXPAND, kernel_size=1, bias=False),
+                    nn.Conv2d(features_out, features_out * FEED_FORWARD_EXPAND, kernel_size=1, bias=False),
                     nn.GELU(),
                     # Feed-forward layer to contract back to the initial number of features
-                    nn.Conv2d(INITIAL_FEATURES * FEED_FORWARD_EXPAND, INITIAL_FEATURES, kernel_size=1, bias=False)
+                    nn.Conv2d(features_out * FEED_FORWARD_EXPAND, features_out, kernel_size=1, bias=False)
                 )
             )
 
         # Final Convolution
-        self.final_conv = nn.Conv2d(INITIAL_FEATURES, 1, kernel_size=1)
+        self.final_conv = nn.Sequential(
+                # stem upscale
+                nn.ConvTranspose2d(LAYER_FEATURES[0], LAYER_FEATURES[0], kernel_size=4, stride=4, bias=False),
+                nn.Conv2d(LAYER_FEATURES[0], 1, kernel_size=1)
+            )
 
     def forward(self, x):
         # Initial Convolution
--- sdf/train.py
+++ sdf/train.py
@@ -15,7 +15,7 @@ def train_model(train_dir: str, callback: Callback = None) -> SDFNet:
     model = SDFNet().to(device)
     # optimizer = optim.Adam(model.parameters(), lr=0.001)
     optimizer = Adafactor(model.parameters())
-    criterion = nn.SmoothL1Loss()
+    criterion = nn.MSELoss()
 
     train_files = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
     train_dataset = SDFDataset(train_files)
